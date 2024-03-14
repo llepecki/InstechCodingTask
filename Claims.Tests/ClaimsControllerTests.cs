f89720e87710a4a323f05e59045db2f9bb7591a5
Łukasz Łepecki <llepecki@users.noreply.github.com>
2024-03-14 11:56:44 +0100

    Integration test for Claims

--- Claims.Tests/ClaimsControllerTests.cs
+++ Claims.Tests/ClaimsControllerTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Claims.Tests
@@ -17,9 +20,49 @@ namespace Claims.Tests
             var response = await client.GetAsync("/Claims");
 
             response.EnsureSuccessStatusCode();
-
-            //TODO: Apart from ensuring 200 OK being returned, what else can be asserted?
         }
 
+        [Fact]
+        public async Task Post_Get_Delete_Claim()
+        {
+            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(_ => {});
+            var client = application.CreateClient();
+
+            var now = DateTime.UtcNow;
+
+            // Create cover
+            var newCover = new CoverWriteModel(
+                DateOnly.FromDateTime(now),
+                DateOnly.FromDateTime(now.AddDays(90)),
+                CoverType.Yacht);
+            
+            var newCoverContent = new StringContent(JsonConvert.SerializeObject(newCover));
+
+            var response = await client.PostAsync("/Covers", newCoverContent);
+            var createdCover = await response.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<CoverReadModel>();
+
+            // Create claim
+            var newClaim = new ClaimWriteModel(
+                createdCover.Id,
+                now.AddDays(30),
+                "TestClaim",
+                ClaimType.Fire,
+                999);
+
+            var newClaimContent = new StringContent(JsonConvert.SerializeObject(newClaim));
+
+            response = await client.PostAsync("/Claims", newClaimContent);
+            var createdClaim = await response.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<ClaimReadModel>();
+
+            Assert.Equal("TestClaim", createdClaim.Name);
+            Assert.Equal(ClaimType.Fire, createdClaim.Type);
+            Assert.Equal(999, createdClaim.DamageCost);
+
+            // Delete claim
+            response = await client.DeleteAsync($"/Claims/{createdClaim.Id}");
+            response = await client.GetAsync($"/Claims/{createdClaim.Id}");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
     }
 }
