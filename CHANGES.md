6366d1ec351764ca66d58ad794ff986dba78b28f
Łukasz Łepecki <llepecki@users.noreply.github.com>
2024-03-14 16:21:58 +0100

    Added description of changes that were introduced

---
+++ CHANGES.md
@@ -0,0 +1,36 @@
+### Task 1
+
+- Divided models into write models, read models and db models. These models should live separately in order to have control over what is shared with the outside world. The second thing is that write models (used in POST) have their ids filled by the infrastructure so the client should be mislead that they should provide the id.
+- Mapping taken away from the controllers (`ToDbModel`, `FromDbModel`)
+- Created generic `CosmosRepository` to encapsulate access to the database.
+- Premium computation moved to a separate service.
+- Added Swagger documentation.
+- Changes some of the API responses: `404 NotFound` when the resource is missing, `201 Created` (with link to the resource in headers and the reource itself in body) when new resource is created, `204 NoContent` when no resource is found and on deletion.
+- Dropped `Async` suffix from controller methods because `CreatedAtAction` has problem generating the reponse if method name end with `Async` (weird, right?).
+- `Get` and `GetAll` method so ther's no amibiguity for method to generate header link when using `CreatedAtAction`.
+- Used `ActionResult<>` for better communication and documentation.
+- Null checks in controllers redundant - DI takes care of that.
+- TODO: improve error handling by logging and fine tuning API responses.
+
+### Task 2
+
+- Simple validations done by implementing `IValidatableObject` interface in write models.
+- Claim against Cover implemented in controller.
+- TODO: Consider using `FluentValidations` to gather all validations in one place.
+
+### Task 3
+- Delegated saving audits to a background service that persists them periodically and before that keeps them in memory.
+- Implemented basic failsafe to flush pending audits on unexpected service crash.
+- TODO: implement rollback mechanism to remove audit in case Cosmos operation fails. On exception from Cosmos there should be audit deltion request in the catch block to assure better data consistency.
+
+### Task 4
+- Implemented 1 full integration test for claims: create, delete, check if deleted.
+- TODO: similar test for covers, test `BadRequst` in case of invalid data.
+
+### Task 5
+- Told Github Copilot to write relevant tests first and then implemented changes in the `ComputePremium` (guess it's AI TDD)
+
+### Other
+- You do know that I'd never commit straight to `master` in the real life, right?
+- Used `record` with either constructor or `init` initialization to have immutable data structure.
+- Cleaned startup up: made whole thing `async`, removed blocking database initialization and moved it just before the app start.
--- Claims.Tests/ClaimsControllerTests.cs
+++ Claims.Tests/ClaimsControllerTests.cs
@@ -6,73 +6,79 @@ using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
-namespace Claims.Tests
+namespace Claims.Tests;
+
+public class ClaimsControllerTests
 {
-    public class ClaimsControllerTests
+    private readonly JsonSerializerOptions _serializerOptions = new JsonSerializerOptions();
+
+    public ClaimsControllerTests()
     {
-        private readonly JsonSerializerOptions _serializerOptions = new JsonSerializerOptions();
-
-        public ClaimsControllerTests()
-        {
-            _serializerOptions.Converters.Add(new JsonStringEnumConverter());
-        }
-
-
-        [Fact]
-        public async Task Get_Claims()
-        {
-            var application = new WebApplicationFactory<Program>()
-                .WithWebHostBuilder(_ =>
-                {});
-
-            var client = application.CreateClient();
-
-            var response = await client.GetAsync("/Claims");
-
-            response.EnsureSuccessStatusCode();
-        }
-
-        [Fact]
-        public async Task Post_Get_Delete_Claim()
-        {
-            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(_ => {});
-            var client = application.CreateClient();
-
-            var now = DateTime.UtcNow;
-
-            // Create cover
-            var newCover = new CoverWriteModel(
-                DateOnly.FromDateTime(now),
-                DateOnly.FromDateTime(now.AddDays(90)),
-                CoverType.Yacht);
-            
-            var newCoverContent = new StringContent(JsonSerializer.Serialize(newCover, _serializerOptions), Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync("/Covers", newCoverContent);
-            var createdCover = await response.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<CoverReadModel>(_serializerOptions);
-
-            // Create claim
-            var newClaim = new ClaimWriteModel(
-                createdCover.Id,
-                now.AddDays(30),
-                "TestClaim",
-                ClaimType.Fire,
-                999);
-
-            var newClaimContent = new StringContent(JsonSerializer.Serialize(newClaim, _serializerOptions), Encoding.UTF8, "application/json");
-
-            response = await client.PostAsync("/Claims", newClaimContent);
-            var createdClaim = await response.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<ClaimReadModel>(_serializerOptions);
-
-            Assert.Equal("TestClaim", createdClaim.Name);
-            Assert.Equal(ClaimType.Fire, createdClaim.Type);
-            Assert.Equal(999, createdClaim.DamageCost);
-
-            // Delete claim
-            response = await client.DeleteAsync($"/Claims/{createdClaim.Id}");
-            response = await client.GetAsync($"/Claims/{createdClaim.Id}");
-
-            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-        }
+        _serializerOptions.Converters.Add(new JsonStringEnumConverter());
     }
+
+
+    [Fact]
+    public async Task Get_Claims()
+    {
+        var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(_ =>
+            {});
+
+        var client = application.CreateClient();
+
+        var response = await client.GetAsync("/Claims");
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task Post_Get_Delete_Claim()
+    {
+        var application = new WebApplicationFactory<Program>().WithWebHostBuilder(_ => {});
+        var client = application.CreateClient();
+
+        var now = DateTime.UtcNow;
+
+        // Create cover
+        var newCover = new CoverWriteModel(
+            DateOnly.FromDateTime(now),
+            DateOnly.FromDateTime(now.AddDays(90)),
+            CoverType.Yacht);
+        
+        var newCoverContent = new StringContent(JsonSerializer.Serialize(newCover, _serializerOptions), Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync("/Covers", newCoverContent);
+        var createdCover = await response.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<CoverReadModel>(_serializerOptions);
+
+        // Create claim
+        var newClaim = new ClaimWriteModel(
+            createdCover!.Id,
+            now.AddDays(30),
+            "TestClaim",
+            ClaimType.Fire,
+            999);
+
+        var newClaimContent = new StringContent(JsonSerializer.Serialize(newClaim, _serializerOptions), Encoding.UTF8, "application/json");
+
+        response = await client.PostAsync("/Claims", newClaimContent);
+        var createdClaim = await response.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<ClaimReadModel>(_serializerOptions);
+
+        Assert.Equal("TestClaim", createdClaim!.Name);
+        Assert.Equal(ClaimType.Fire, createdClaim.Type);
+        Assert.Equal(999, createdClaim.DamageCost);
+
+        // Delete claim
+        response = await client.DeleteAsync($"/Claims/{createdClaim.Id}");
+        response = await client.GetAsync($"/Claims/{createdClaim.Id}");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    // TODO: Write more tests:
+    // Test BadRequest when validation should fail
+    // Test getting multiple claims
+    // Test saving audits
+
+    // TODO: Implement db cleanup using IAsyncLifetime
 }
